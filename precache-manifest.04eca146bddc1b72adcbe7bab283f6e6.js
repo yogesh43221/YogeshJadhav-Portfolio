9be3bb1b360a26e557fce050554837d470b1a9bd
Yogesh Jadhav <yogesh.jadhav@mitaoe.ac.in>
2024-05-20 16:34:46 +0530

    Updates

---
+++ precache-manifest.04eca146bddc1b72adcbe7bab283f6e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754ecc801aec6f3d4a7766646af620ef",
+    "url": "/YogeshJadhav-Portfolio/index.html"
+  },
+  {
+    "revision": "e90060b8766f666eea78",
+    "url": "/YogeshJadhav-Portfolio/static/css/main.22718620.chunk.css"
+  },
+  {
+    "revision": "7af1697b51c6feb648d0",
+    "url": "/YogeshJadhav-Portfolio/static/js/2.85b54ad9.chunk.js"
+  },
+  {
+    "revision": "e90060b8766f666eea78",
+    "url": "/YogeshJadhav-Portfolio/static/js/main.ffff6bed.chunk.js"
+  },
+  {
+    "revision": "57330d3079d409f0e42a",
+    "url": "/YogeshJadhav-Portfolio/static/js/runtime~main.36245bcc.js"
+  },
+  {
+    "revision": "27c14b1dd59bf75460eac95f999a7737",
+    "url": "/YogeshJadhav-Portfolio/static/media/HeroImage.27c14b1d.jpg"
+  }
+]);
